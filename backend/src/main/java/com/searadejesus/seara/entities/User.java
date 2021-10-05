d37938efd3d480d5bb37b5612c257713cca952a8
Marcelo Gomes <marcelosuptec@hotmail.com>
2021-10-05 06:59:09 -0300

    Concluídas as entidades

--- backend/src/main/java/com/searadejesus/seara/entities/User.java
+++ backend/src/main/java/com/searadejesus/seara/entities/User.java
@@ -1,7 +1,9 @@
 package com.searadejesus.seara.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -32,6 +35,9 @@ public class User implements Serializable {
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Psychography> psychographys = new ArrayList<>();
 
     public User() {
     }
@@ -86,6 +92,13 @@ public class User implements Serializable {
 
     public Set<Role> getRoles() {
         return this.roles;
+    }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<Psychography> getPsychographys() {
+        return this.psychographys;
     }
 
     @Override
